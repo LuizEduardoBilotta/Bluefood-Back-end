057126a1d7b6147f547968b4c1e1b97c7ff716ba
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-07-17 09:09:00 -0300

    AULA 11.6 - Criando um layout genérico das páginas

---
+++ src/main/java/br/com/bilotta/bluefood/infrastructure/web/controller/LoginController.java
@@ -0,0 +1,13 @@
+package br.com.bilotta.bluefood.infrastructure.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping(path = { "/login", "/" })
+	public String login() {
+		return "login";
+	}
+}
