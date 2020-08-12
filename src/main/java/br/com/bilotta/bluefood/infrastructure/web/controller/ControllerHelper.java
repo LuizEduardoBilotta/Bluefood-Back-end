fc3ce3d8076d0ba87da662a6e551f97c9d1a8c90
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-08-12 19:18:59 -0300

    AULA 12.3 - Montando uma caixa de seleção dinâmica de categorias

--- src/main/java/br/com/bilotta/bluefood/infrastructure/web/controller/ControllerHelper.java
+++ src/main/java/br/com/bilotta/bluefood/infrastructure/web/controller/ControllerHelper.java
@@ -1,10 +1,21 @@
 package br.com.bilotta.bluefood.infrastructure.web.controller;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.ui.Model;
+
+import br.com.bilotta.bluefood.domain.restaurante.CategoriaRestaurante;
+import br.com.bilotta.bluefood.domain.restaurante.CategoriaRestauranteRepository;
 
 public class ControllerHelper {
 	
 	public static void setEditMode(Model model, boolean isEdit) {
 		model.addAttribute("editMode", isEdit);
+	}
+	
+	public static void addCategoriasToRequest(CategoriaRestauranteRepository repository, Model model) {
+		List<CategoriaRestaurante> categorias = repository.findAll(Sort.by("nome"));
+		model.addAttribute("categorias", categorias);
 	}
 }
--- src/main/java/br/com/bilotta/bluefood/infrastructure/web/controller/PublicController.java
+++ src/main/java/br/com/bilotta/bluefood/infrastructure/web/controller/PublicController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import br.com.bilotta.bluefood.application.ClienteService;
 import br.com.bilotta.bluefood.application.ValidationException;
 import br.com.bilotta.bluefood.domain.cliente.Cliente;
+import br.com.bilotta.bluefood.domain.restaurante.CategoriaRestauranteRepository;
 import br.com.bilotta.bluefood.domain.restaurante.Restaurante;
 
 @Controller
@@ -22,6 +23,9 @@ public class PublicController {
 
 	@Autowired
 	private ClienteService clienteService;
+	
+	@Autowired
+	private CategoriaRestauranteRepository categoriaRestauranteRepository;
 	
 	@GetMapping("cliente/new")
 	public String newCliente(Model model) {
@@ -34,6 +38,7 @@ public class PublicController {
 	public String newRestaurante(Model model) {
 		model.addAttribute("restaurante", new Restaurante());
 		ControllerHelper.setEditMode(model, false);
+		ControllerHelper.addCategoriasToRequest(categoriaRestauranteRepository, model);
 		return "restaurante-cadastro";
 	}
 	
