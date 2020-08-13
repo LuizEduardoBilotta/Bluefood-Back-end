386a35a2e92cdf2dcc46e0a2b9a17175e6a0eebc
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-08-13 20:45:46 -0300

    AULA 12.10 - Configurando a segurança da aplicação - Parte 2

--- src/main/java/br/com/bilotta/bluefood/infrastructure/web/controller/LoginController.java
+++ src/main/java/br/com/bilotta/bluefood/infrastructure/web/controller/LoginController.java
@@ -1,6 +1,7 @@
 package br.com.bilotta.bluefood.infrastructure.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,6 +9,12 @@ public class LoginController {
 	
 	@GetMapping(path = { "/login", "/" })
 	public String login() {
+		return "login";
+	}
+	
+	@GetMapping(path = { "/login-error" })
+	public String loginError (Model model) {
+		model.addAttribute("Credenciais inválidas!");
 		return "login";
 	}
 }
---
+++ src/main/java/br/com/bilotta/bluefood/util/SecurityUtils.java
@@ -0,0 +1,50 @@
+package br.com.bilotta.bluefood.util;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import br.com.bilotta.bluefood.domain.cliente.Cliente;
+import br.com.bilotta.bluefood.domain.restaurante.Restaurante;
+import br.com.bilotta.bluefood.infrastructure.web.security.LoggedUser;
+
+public class SecurityUtils {
+
+	public static LoggedUser loggedUser() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		if (authentication instanceof AnonymousAuthenticationToken) {
+			return null;
+		}
+		
+		return (LoggedUser)authentication.getPrincipal();
+	}
+	
+	public static Cliente loggedCliente() {
+		LoggedUser loggedUser = loggedUser();
+		
+		if (loggedUser == null) {
+			throw new IllegalStateException("Não existe um usuário logado!");
+		}
+		
+		if(!(loggedUser.getUsuario() instanceof Cliente)) {
+			throw new IllegalStateException("O usuário logado não é um cliente!");
+		}
+		
+		return (Cliente) loggedUser.getUsuario();
+	}
+	
+	public static Restaurante loggedRestaurante() {
+		LoggedUser loggedUser = loggedUser();
+		
+		if (loggedUser == null) {
+			throw new IllegalStateException("Não existe um usuário logado!");
+		}
+		
+		if(!(loggedUser.getUsuario() instanceof Restaurante)) {
+			throw new IllegalStateException("O usuário logado não é um restaurante!");
+		}
+		
+		return (Restaurante) loggedUser.getUsuario();
+	}
+}
