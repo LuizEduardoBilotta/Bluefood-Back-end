c2bf62942f30946f51a31ef19336514586c5b89c
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-08-14 13:42:59 -0300

    AULA 12.13 - Home do cliente: alteração de dados e logout

---
+++ src/main/java/br/com/bilotta/bluefood/infrastructure/web/controller/ClienteController.java
@@ -0,0 +1,63 @@
+package br.com.bilotta.bluefood.infrastructure.web.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.bilotta.bluefood.application.service.ClienteService;
+import br.com.bilotta.bluefood.application.service.ValidationException;
+import br.com.bilotta.bluefood.domain.cliente.Cliente;
+import br.com.bilotta.bluefood.domain.cliente.ClienteRepository;
+import br.com.bilotta.bluefood.util.SecurityUtils;
+
+@Controller
+@RequestMapping(path = "/cliente")
+public class ClienteController {
+	
+	@Autowired
+	private ClienteRepository ClienteRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@GetMapping(path = "/home")
+	public String home() {
+		return "cliente-home";
+	}
+	
+	@GetMapping("/edit")
+	public String edit(Model model) {
+		Integer clienteId = SecurityUtils.loggedCliente().getId();
+		Cliente cliente = ClienteRepository.findById(clienteId).orElseThrow();
+		model.addAttribute("cliente", cliente);
+		ControllerHelper.setEditMode(model, true);
+		
+		return "cliente-cadastro";
+	}
+	
+	@PostMapping("/save")
+	public String save(@ModelAttribute("cliente") @Valid Cliente cliente,
+		Errors errors,
+		Model model) {
+	
+	if(!errors.hasErrors()) {
+		try {
+			clienteService.saveCliente(cliente);
+			model.addAttribute("msg", "Cliente gravado com sucesso!");
+		} catch (ValidationException e){
+			errors.rejectValue("email", null, e.getMessage());
+		}
+	}
+	
+	ControllerHelper.setEditMode(model, true);
+	return "cliente-cadastro";
+	}
+	
+}
