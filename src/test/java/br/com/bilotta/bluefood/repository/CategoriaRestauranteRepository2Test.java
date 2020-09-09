3125f96070926a3cabe8e336d9790264d0779d0f
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-09-09 18:28:29 -0300

    AULA_17.3 - Criando testes para repositórios (outra forma de fazer)

---
+++ src/test/java/br/com/bilotta/bluefood/repository/CategoriaRestauranteRepository2Test.java
@@ -0,0 +1,41 @@
+package br.com.bilotta.bluefood.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import br.com.bilotta.bluefood.domain.restaurante.CategoriaRestaurante;
+import br.com.bilotta.bluefood.domain.restaurante.CategoriaRestauranteRepository;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class CategoriaRestauranteRepository2Test {
+
+	@Autowired
+	private TestEntityManager em;
+	
+	@Test
+	public void testInsertAndDelete() throws Exception {
+		
+		assertThat(em).isNotNull();
+		
+		CategoriaRestaurante cr = new CategoriaRestaurante();
+		cr.setNome("Chinesa");
+		cr.setImagem("chinesa.png");
+		em.persistAndFlush(cr);
+		
+		assertThat(cr.getId()).isNotNull();
+		
+		CategoriaRestaurante cr2 = em.find(CategoriaRestaurante.class, cr.getId());
+		assertThat(cr.getNome()).isEqualTo(cr2.getNome());
+				
+		em.remove(cr);	
+		assertThat(em.find(CategoriaRestaurante.class, cr.getId())).isNull();
+	}
+}
